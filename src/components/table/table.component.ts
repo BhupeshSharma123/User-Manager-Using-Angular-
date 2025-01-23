c0742449c85eff31cc11508df8536d2d0e6e1382
John Doe <johndoe@email.com>
2025-01-23 11:27:49 +0530

    global filter and column filter applied

--- src/components/table/table.component.ts
+++ src/components/table/table.component.ts
@@ -5,11 +5,12 @@ import { EditUserDialogComponent } from '../models/edit-user-dialog/edit-user-di
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from '../models/confirmation-dialog/confirmation-dialog.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-table',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   providers: [ApiServicesService],
-  // tslint:disable-next-line:component-selector
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
 })
@@ -18,57 +19,13 @@ export class TableComponent implements OnInit {
   isAscending: boolean = true; // Sort direction
   users: any[] = [];
   currentPage: number = 1;
-  sortedData!: any[];
-  // for Deleting columns
-  openConfirmationDialog(user: any): void {
-    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      width: '350px',
-      data: { message: 'Are you sure you want to proceed?' },
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result) {
-        console.log('User confirmed the action');
-        this.deleteUser(user);
-        this.showToast('User Deleted');
-        // Perform the action (e.g., save, delete, etc.)
-      } else {
-        console.log('User canceled the action');
-      }
-    });
-  }
-  // for Edit user dialog
-  editUser(user: any) {
-    this.apiService.updateData(user.id, user);
-    this.fetchData();
-    console.log(user);
-
-    const dialogRef = this.dialog.open(EditUserDialogComponent, {
-      width: '400px',
-      data: { ...user }, // Pass a copy of the user data to the dialog
-    });
-    dialogRef.afterClosed().subscribe((result: any) => {
-      if (result) {
-        // Update the user data
-        const index = this.users.findIndex((u) => u.id === user.id);
-        if (index !== -1) {
-          this.users[index] = result;
-        }
-      }
-    });
-  }
-  addUser(user: any) {
-    this.apiService.addUser(user);
-    this.fetchData();
-    console.log(user);
-  }
-  deleteUser(user: any) {
-    this.apiService.deleteData(user.id);
-
-    console.log('Delete user:', user);
-
-    this.users = this.users.filter((u) => u.id !== user.id);
-  }
+  filteredUsers: any[] = []; // Initialize as empty array
+  columnFilters: { [key: string]: string } = {
+    id: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+  };
 
   constructor(
     private snackBar: MatSnackBar,
@@ -83,40 +40,12 @@ export class TableComponent implements OnInit {
   fetchData() {
     this.apiService.getData(this.currentPage).subscribe((response: any) => {
       this.users = response.data;
-      const usersData = response.data;
-      console.log(usersData);
+      this.filteredUsers = [...this.users]; // Update filteredUsers with new data
+      console.log('Fetched data:', this.users);
     });
   }
 
-  // Toggle Sorting
-  sortData(column: string): void {
-    if (this.currentSort === column) {
-      // Reverse if already sorted by this column
-      this.users.reverse();
-    } else {
-      // Sort by the selected column
-      this.currentSort = column;
-      this.users.sort((a, b) => {
-        const valueA = a[column];
-        const valueB = b[column];
-
-        return valueA > valueB ? 1 : valueA < valueB ? -1 : 0;
-      });
-    }
-  }
-  showToast(
-    message: string,
-    action: string = 'Close',
-    duration: number = 3000
-  ): void {
-    this.snackBar.open(message, action, {
-      duration: duration, // Duration in milliseconds
-      horizontalPosition: 'right', // Position horizontally
-      verticalPosition: 'top', // Position vertically
-    });
-  }
-  // Filtering Data
-
+  // Pagination
   nextPage() {
     this.currentPage++;
     this.fetchData();
@@ -127,5 +56,95 @@ export class TableComponent implements OnInit {
       this.currentPage--;
       this.fetchData();
     }
+  }
+
+  // Sorting
+  sortData(column: string): void {
+    if (this.currentSort === column) {
+      this.filteredUsers.reverse(); // Reverse if already sorted by this column
+    } else {
+      this.currentSort = column;
+      this.filteredUsers.sort((a, b) => {
+        const valueA = a[column];
+        const valueB = b[column];
+        return valueA > valueB ? 1 : valueA < valueB ? -1 : 0;
+      });
+    }
+  }
+
+  // Filtering
+  applyColumnFilter(column: string, value: string): void {
+    this.columnFilters[column] = value.toLowerCase();
+    this.filteredUsers = this.users.filter((user) =>
+      Object.keys(this.columnFilters).every((key) =>
+        this.columnFilters[key]
+          ? user[key].toString().toLowerCase().includes(this.columnFilters[key])
+          : true
+      )
+    );
+  }
+
+  applyGlobalFilter(searchText: string): void {
+    const lowerCaseSearch = searchText.toLowerCase();
+    this.filteredUsers = this.users.filter((user) =>
+      Object.values(user).some((value: any) =>
+        value.toString().toLowerCase().includes(lowerCaseSearch)
+      )
+    );
+  }
+
+  // Edit User
+  editUser(user: any) {
+    const dialogRef = this.dialog.open(EditUserDialogComponent, {
+      width: '400px',
+      data: { ...user }, // Pass a copy of the user data to the dialog
+    });
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result) {
+        // Update the user data
+        const index = this.users.findIndex((u) => u.id === user.id);
+        if (index !== -1) {
+          this.users[index] = result;
+          this.filteredUsers = [...this.users]; // Update filteredUsers
+        }
+      }
+    });
+  }
+
+  // Delete User
+  openConfirmationDialog(user: any): void {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '350px',
+      data: { message: 'Are you sure you want to delete this user?' },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.deleteUser(user);
+        this.showToast('User Deleted');
+      }
+    });
+  }
+
+  deleteUser(user: any) {
+    this.apiService.deleteData(user.id).subscribe(() => {
+      this.users = this.users.filter((u) => u.id !== user.id);
+      this.filteredUsers = [...this.users]; // Update filteredUsers
+      console.log('User deleted:', user);
+    });
+  }
+
+  // Show Toast Notification
+  showToast(
+    message: string,
+    action: string = 'Close',
+    duration: number = 3000
+  ): void {
+    this.snackBar.open(message, action, {
+      duration: duration,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
   }
 }
